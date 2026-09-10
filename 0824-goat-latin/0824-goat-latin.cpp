bf94990097d5b340428afd3ea6a22c9724450d20
Shruti Jha <shrutijha140921@gmail.com>
2026-09-10 16:32:11 +0530

    Time: 3 ms (17.51%), Space: 9.1 MB (49.44%) - LeetHub

---
+++ 0824-goat-latin/0824-goat-latin.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string toGoatLatin(string sentence) {
+        stringstream ss(sentence);
+        string word;
+        string result = "";
+        int index = 1;
+
+        while (ss >> word) {
+            // Check if first character is a vowel
+            char first = tolower(word[0]);
+
+            if (first != 'a' && first != 'e' && 
+                first != 'i' && first != 'o' && 
+                first != 'u') {
+                
+                // Move first character to the end
+                word = word.substr(1) + word[0];
+            }
+
+            // Add "ma"
+            word += "ma";
+
+            // Add 'a' according to word index
+            word += string(index, 'a');
+
+            // Add word to result
+            if (!result.empty()) {
+                result += " ";
+            }
+
+            result += word;
+            index++;
+        }
+
+        return result;
+    }
+};
